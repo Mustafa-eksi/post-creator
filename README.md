05e004c17410286802eb0203466e3860b7a2fbc7
Mustafa-eksi <mustafa.eskieksi@gmail.com>
2023-05-20 21:02:45 +0300

    Minik tasarım değişiklikleri, indir butonunun eklenmesi (sadece yerel dosyalarla çalışıyor) ve birkaç varsayılanı değiştirdim.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Resime yazı ekleme aracı
+https://mustafa-eksi.github.io/post-creator/dist/index.html
+İsterseniz yerel bir resmi yükleyip ona yazı ekleyebilirsiniz. Veya girdiğiniz subreddit'teki rastgele bir gönderinin resmini ve başlığını kopyalayabilirsiniz. Reddit'ten alınan resimleri indir butonuyla indiremiyorsunuz ama bilgisayarda olanlar sağ tıklayıp resmi farklı kaydet diyebilir.
 Haber paylaşılırken sık sık kullanılan bir tarzı kolaylaştırmak için yaptım, bir çok değişiklik olabilir, yakında kodu daha anlaşılır yapacağım. Reddit aracı rastgele post çekiyor ama twitter linkiyle de yapmayı eklemeyi düşünüyorum. Pull-request veya issue açmaktan çekinmeyin.
 Ürettiğiniz resme sağ tıkayıp "Save Image As..." butonuyla kaydedebilirsiniz.
 İsim bulmakta iyi değilim.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const justreddit = require("justreddit");
+// import {Tweet, Scraper} from '@the-convocation/twitter-scraper';
 var canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 var CanvasTextWrapper = require('canvas-text-wrapper').CanvasTextWrapper;
+// var tweetScraper = new Scraper();
 
+const indir = document.getElementById("indir");
 const git = document.getElementById("git");
 const sub = document.getElementById("subreddit");
+// const link = document.getElementById("link");
+// const linkgit = document.getElementById("linkgit");
 const img_padding = document.getElementById("img-padding"); img_padding.value = "30";
 const html_text = document.getElementById("text");
 const yenile = document.getElementById("yenile");
 const text_size = document.getElementById("text-size"); text_size.value = "30";
-const text_font = document.getElementById("text-font"); text_font.value = "OpenSans";
+const text_font = document.getElementById("text-font"); text_font.value = "Arial";
 const text_color = document.getElementById("text-color"); text_color.value = "#FFFFFF";
 const gradient_color = document.getElementById("gradient-color"); gradient_color.value = "#0000FF";
 const gradient_check = document.getElementById("gradient-check"); gradient_check.ariaChecked = true;
@@ -31,31 +36,49 @@ async function getRandomPost(subreddit) {
   }
 }
 
-function PutTextOnImage(url, text, options, imagePadding, textColor, gradientColor, gradient, gradiantStart) {
+// linkgit.addEventListener('click', async()=>{
+//   let tweet = await Tweet.getTweet(link.value, false, tweetScraper.auth);
+//   console.log("tweet:");
+//   console.log(tweet);
+// })
+
+var ikili = document.getElementById("ikili");
+async function PutTextOnImage(url, text, options, imagePadding, textColor, gradientColor, gradient, gradiantStart) {
+  ikili.removeChild(canvas);
+  canvas = document.createElement('canvas');
+  canvas.id = "canvas"; canvas.height = 500; canvas.width = 500;
+  ctx = canvas.getContext('2d');
+  ikili.appendChild(canvas);
   var img = new Image();
   img.onload = async function() {
     ctx.canvas.height = img.height+imagePadding;
     ctx.canvas.width = img.width;
-    ctx.drawImage(await createImageBitmap(img), 0, 0);
+    ctx.drawImage(img, 0, 0);
     if(gradient === true) {
       console.log(parseInt(ctx.canvas.height*(1-gradiantStart)), gradiantStart);
       const gradient = ctx.createLinearGradient(0, parseInt(ctx.canvas.height*(1-gradiantStart)), 0, ctx.canvas.height);
       gradient.addColorStop(0, 'transparent');     // Start color
       gradient.addColorStop(1, gradientColor);    // End color
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    }else {
-      ctx.fillStyle = gradientColor;
-      ctx.fillRect(0, img.height, canvas.width, canvas.height);
+      ctx.fillStyle = gradient;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
+    ctx.fillStyle = gradientColor;
+    ctx.fillRect(0, img.height, canvas.width, canvas.height);
     ctx.fillStyle = textColor;
     CanvasTextWrapper(ctx.canvas, text, options)
   };
   img.src = url;
 }
 
+indir.addEventListener('click', ()=>{
+  let a = document.createElement("a");
+  a.download = "post."+resim_dosyasi.files[0].name.split(".")[1];
+  a.href = canvas.toDataURL();
+  a.click();
+})
 
 git.addEventListener('click', ()=>{
+  indir.disabled = true;
   getRandomPost(sub.value).then(async(post)=>{
     currentImage = post.image;
     html_text.value = post.title;
@@ -66,14 +89,15 @@ git.addEventListener('click', ()=>{
 })
 
 yenile.addEventListener('click', ()=>{
-  if(resim_dosyasi.files.length !== 0) {
+  if(resim_dosyasi.files.length !== 0 || currentImage === "") {
     if (resim_dosyasi.files && resim_dosyasi.files[0]) {
       var reader = new FileReader();
       reader.onload = function (e) {
         currentImage = e.target.result;
+        PutTextOnImage(currentImage, html_text.value, {font: text_size.value+'px '+text_font.value, verticalAlign:"bottom", paddingX:20, paddingY:20}, parseInt(img_padding.value), text_color.value, gradient_color.value, gradient_check.checked, parseInt(gradient_start.value)/100)
+        indir.disabled = false;
       }
       reader.readAsDataURL(resim_dosyasi.files[0]);
     }
   }
-  PutTextOnImage(currentImage, html_text.value, {font: text_size.value+'px '+text_font.value, verticalAlign:"bottom", paddingX:20, paddingY:20}, parseInt(img_padding.value), text_color.value, gradient_color.value, gradient_check.checked, parseInt(gradient_start.value)/100)
 })
